3990237dd69097c5648ee5eb418bc3a2b336c860
jpablop123 <juanpablopinto123@icloud.com>
2023-02-25 03:48:49 +0000

    hola

---
+++ src/js/component/secondsCounter.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const SecondsCounter = (props) => {
+  let unidadSegundos = props.seconds;
+  let decenasSegundos = 0;
+
+  if (unidadSegundos > 9) {
+    unidadSegundos = 0;
+    decenasSegundos++;
+    unidadSegundos = props.seconds - 10 * decenasSegundos;
+  }
+  return (
+    <>
+    <div className="p-1  bg-dark text-white  ">
+      <div className="container mt-15" >
+        <div className="row" >
+          <div className="col btn btn-secondary mx-1 py-4">
+            <i className="fa-solid fa-clock"></i>{" "}
+          </div>
+
+          <div className="col btn btn-secondary mx-1 py-4">
+            {" "}
+            {props.cienmilesSeconds}{" "}
+          </div>
+          <div className="col btn btn-secondary mx-1 py-4">
+            {" "}
+            {props.diezmilesSeconds}{" "}
+          </div>
+       
+          <div className="col btn btn-secondary mx-1 py-4">
+            {" "}
+            {props.centenaSeconds}
+          </div>
+          <div className="col btn btn-secondary mx-1 py-4">
+            {" "}
+            {props.decenaSeconds}{" "}
+          </div>
+          <div className="col btn btn-secondary mx-1 py-4"> {props.seconds} </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,51 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+import SecondsCounter from "./component/secondsCounter.jsx";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let tiempoSegundos = 0;
+let decenaSeconds = 0;
+let centenaSeconds = 0;
+let milesSeconds = 0;
+let diezmilesSeconds = 0;
+let cienmilesSeconds = 0 ;
+
+setInterval(() => {
+  tiempoSegundos = tiempoSegundos + 1;
+
+  if(tiempoSegundos % 10 == 0 && tiempoSegundos != 0){
+    tiempoSegundos = 0
+    decenaSeconds++
+  }
+  if(decenaSeconds % 10 == 0 && decenaSeconds != 0){
+    decenaSeconds = 0
+    centenaSeconds++
+    
+    }
+    if(centenaSeconds % 10 == 0 && centenaSeconds != 0){
+        centenaSeconds = 0
+        milesSeconds++  }
+
+
+    if(milesSeconds % 10 == 0 && milesSeconds != 0){
+        milesSeconds = 0
+        diezmilesSeconds++  }
+
+        if(diezmilesSeconds % 10 == 0 && diezmilesSeconds != 0){
+            diezmilesSeconds = 0
+            cienmilesSeconds++  }
+
+          
+            
+            
+           
+    
+
+
+ReactDOM.render(
+    <SecondsCounter centenaSeconds={centenaSeconds}  milesSeconds={milesSeconds} diezmilesSeconds={diezmilesSeconds} decenaSeconds={decenaSeconds} seconds={tiempoSegundos} cienmilesSeconds={cienmilesSeconds} />,
+    document.querySelector("#app")
+  );
+}, 100);
+
+
